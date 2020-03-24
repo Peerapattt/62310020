03d372c58b3148653b2f98c818b8df68331bcc53
Peerapattt <59908940+Peerapattt@users.noreply.github.com>
2020-03-24 10:19:49 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310020"; ?><br>
+<?php echo "ชื่อ-สกุล นายพีรพัฒน์ เยียรยงค์"; ?><br>
+<iframe width="500" height="500"
+src="https://www.youtube.com/watch?v=RUx5PtCmlX8">
+</iframe>
+</BODY>
+</HTML>
