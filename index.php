8cf9ae5039cfbcbd4ee97ddfd705f32aa8b40045
Peerapattt <59908940+Peerapattt@users.noreply.github.com>
2020-03-24 10:48:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php echo "รหัสนิสิต-62310020"; ?><br>
 <?php echo "ชื่อ-สกุล นายพีรพัฒน์ เยียรยงค์"; ?><br>
 <iframe width="500" height="500"
-src="https://www.youtube.com/watch?v=RUx5PtCmlX8">
+src="https://www.youtube.com/embed/RUx5PtCmlX8?autoplay=1%22%3E
 </iframe>
 </BODY>
 </HTML>
